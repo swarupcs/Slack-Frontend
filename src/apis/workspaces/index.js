9bb3985c44017c22cc964461032fbba7eb1cc9e4
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-04 16:54:34 +0530

    Added get Workspace by id api

--- src/apis/workspaces/index.js
+++ src/apis/workspaces/index.js
@@ -33,3 +33,18 @@ export const fetchWorkspacesRequest = async ({ token }) => {
     throw error.response.data;
   }
 };
+
+
+export const fetchWorkspaceDetailsRequest = async ({ workspaceId, token }) => {
+  try {
+    const response = await axios.get(`/workspaces/${workspaceId}`, {
+      headers: {
+        'x-access-token': token,
+      },
+    });
+    return response?.data?.data;
+  } catch (error) {
+    console.log('Error in fetching workspace details request', error);
+    throw error.response.data;
+  }
+};
