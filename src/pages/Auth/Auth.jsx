e0488a8b39d00205f5af51b9930128ea5fcb04b7
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-04 10:22:41 +0530

    Added custom slack color to tailwind

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,7 +2,7 @@ import { SignupCard } from '@/components/organisms/Auth/SignupCard';
 
 export const Auth = () => {
   return (
-    <div className='h-[100vh] flex items-center justify-center bg-[#5c3B58]'>
+    <div className='h-[100vh] flex items-center justify-center bg-[var(--slack-color)]'>
       <div className='md:h-auto md:w-[420px]'>
         <SignupCard />
       </div>
