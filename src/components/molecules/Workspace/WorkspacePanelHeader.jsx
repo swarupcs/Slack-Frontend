760ce74e7b6cac3928b85c43200f0e21b1a2d64c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-08 21:58:44 +0530

    Added invite modal

--- src/components/molecules/Workspace/WorkspacePanelHeader.jsx
+++ src/components/molecules/Workspace/WorkspacePanelHeader.jsx
@@ -1,6 +1,7 @@
 import { ChevronDownIcon, ListFilterIcon, SquarePenIcon } from 'lucide-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import { WorkspaceInviteModal } from '@/components/organisms/Modals/WorkspaceInviteModal';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -14,6 +15,8 @@ import { useWorkspacePreferencesModal } from '@/hooks/context/useWorkspacePrefer
 
 export const WorkspacePanelHeader = ({ workspace }) => {
   console.log('workspace is', workspace);
+
+  const [openInviteModal, setOpenInviteModal] = useState(false);
 
   const { setWorkspace } = useWorkspacePreferencesModal();
 
@@ -39,58 +42,74 @@ export const WorkspacePanelHeader = ({ workspace }) => {
   }, []);
 
   return (
-    <div className='flex items-center justify-between px-4 h-[50px] gap-0.5'>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button
-            variant='transparent'
-            className='font-semibold text-lg w-auto p-1.5 overflow-hidden'
-          >
-            <span className='truncate'>{workspace?.name}</span>
-            <ChevronDownIcon className='size-5 ml-1' />
+    <>
+      <WorkspaceInviteModal
+        openInviteModal={openInviteModal}
+        setOpenInviteModal={setOpenInviteModal}
+        workspaceName={workspace?.name}
+        joinCode={workspace?.joinCode}
+      />
+
+      <div className='flex items-center justify-between px-4 h-[50px] gap-0.5'>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button
+              variant='transparent'
+              className='font-semibold text-lg w-auto p-1.5 overflow-hidden'
+            >
+              <span className='truncate'>{workspace?.name}</span>
+              <ChevronDownIcon className='size-5 ml-1' />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent side='bottom' align='start' className='w-64'>
+            <DropdownMenuItem>
+              <div className='size-9 relative overflow-hidden text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2 bg-[#616061]'>
+                {workspace?.name.charAt(0).toUpperCase()}
+              </div>
+              <div className='flex flex-col items-start'>
+                <p className='font-bold'>{workspace?.name}</p>
+                <p className='text-xs text-muted-foreground'>
+                  Active Workspace
+                </p>
+              </div>
+            </DropdownMenuItem>
+
+            {isLoggedInUserAdminOfWorkspace && (
+              <>
+                <DropdownMenuItem
+                  className='cursor-pointer py-2'
+                  onClick={() => {
+                    setInitialValue(workspace?.name);
+                    setOpenPreferences(true);
+                  }}
+                >
+                  Preferences
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className='cursor-pointer py-2'
+                  onClick={() => {
+                    setOpenInviteModal(true);
+                  }}
+                >
+                  Invite people to {workspace?.name}
+                </DropdownMenuItem>
+              </>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <div className='flex items-center gap-0.5'>
+          <Button variant='transparent' size='iconSm'>
+            <ListFilterIcon className='size-5' />
           </Button>
-        </DropdownMenuTrigger>
 
-        <DropdownMenuContent side='bottom' align='start' className='w-64'>
-          <DropdownMenuItem>
-            <div className='size-9 relative overflow-hidden text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2 bg-[#616061]'>
-              {workspace?.name.charAt(0).toUpperCase()}
-            </div>
-            <div className='flex flex-col items-start'>
-              <p className='font-bold'>{workspace?.name}</p>
-              <p className='text-xs text-muted-foreground'>Active Workspace</p>
-            </div>
-          </DropdownMenuItem>
-
-          {isLoggedInUserAdminOfWorkspace && (
-            <>
-              <DropdownMenuItem
-                className='cursor-pointer py-2'
-                onClick={() => {
-                  setInitialValue(workspace?.name);
-                  setOpenPreferences(true);
-                }}
-              >
-                Preferences
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem className='cursor-pointer py-2'>
-                Invite people to {workspace?.name}
-              </DropdownMenuItem>
-            </>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-      <div className='flex items-center gap-0.5'>
-        <Button variant='transparent' size='iconSm'>
-          <ListFilterIcon className='size-5' />
-        </Button>
-
-        <Button variant='transparent' size='iconSm'>
-          <SquarePenIcon className='size-5' />
-        </Button>
+          <Button variant='transparent' size='iconSm'>
+            <SquarePenIcon className='size-5' />
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
