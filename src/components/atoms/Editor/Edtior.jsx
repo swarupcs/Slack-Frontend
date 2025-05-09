7fa8ebc0299f926dde7de69b20bf2b887cae260f
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-09 22:34:12 +0530

    Added a icon for toggle toolbar

--- src/components/atoms/Editor/Edtior.jsx
+++ src/components/atoms/Editor/Edtior.jsx
@@ -2,6 +2,10 @@ import 'quill/dist/quill.snow.css'; // ES6
 
 import Quill from 'quill';
 import { useEffect, useRef, useState } from 'react';
+
+import { PiTextAa } from 'react-icons/pi';
+
+import { Button } from '@/components/ui/button';
 export const Editor = ({
   variant = 'create',
   onSubmit,
@@ -10,9 +14,20 @@ export const Editor = ({
   disabled,
   defaultValue,
 }) => {
+
+    const [isToolbarVisible, setIsToolbarVisible] = useState(false);
   const containerRef = useRef(); // reqd to initialize the editor
   const defaultValueRef = useRef();
   const quillRef = useRef();
+
+  function toggleToolbar() {
+    setIsToolbarVisible(!isToolbarVisible);
+    const toolbar = containerRef.current.querySelector('.ql-toolbar');
+    if (toolbar) {
+      toolbar.classList.toggle('hidden');
+    }
+  }
+
 
   useEffect(() => {
     if (!containerRef.current) return; // if containerRef is not initialized, return
@@ -64,6 +79,17 @@ export const Editor = ({
     <div className='flex flex-col'>
       <div className='flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white '>
         <div className='h-full ql-custom' ref={containerRef} />
+
+        <div className='flex px-2 pb-2 z-[5]'>
+          <Button
+            size='iconSm'
+            variant='ghost'
+            disabled={false}
+            onClick={toggleToolbar}
+          >
+            <PiTextAa className='size-4' />
+          </Button>
+        </div>
       </div>
 
       <p className='p-2 text-[10px] text-mutes-foreground flex justify-end'>
